04d24d3f985c37642bee8d74d263ab4272676abd
Rainford Lennon <rainfordlennon@gmail.com>
2022-12-23 06:38:41 -0500

    Add files via upload

---
+++ components/HowItWork.jsx
@@ -0,0 +1,24 @@
+function HowItWork(){
+    return (
+        <section className="how-it-work" id="how_it_work">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-6">
+                        <div className="how-it-work-content">
+                            <h3>How does it work?</h3>
+                            <p>We will have a voting methodology where the community will be able to vote for each destination provided to the community. You choose the locations where you’d like to visit with your family and friends. All destinations can be
+                                voted for via community voting method.
+                            </p>
+                            <p>Each destination has its own entertainment and other cultural events. Our aim is to define Kadena Artwork as a long-term brand by introducing real use case adoption to the NFT space via smart contracts.
+                            </p>
+                            <p>We want to build a community with NFTs that has a meaning and culture behind it. We will bring value to all participants in Kadena Artwork and be more than just an investment.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default HowItWork
