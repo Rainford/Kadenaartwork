fb4bccf24c62e6f5e772ae5900afc11aaea43b35
Rainford Lennon <rainfordlennon@gmail.com>
2023-01-09 21:51:40 -0500

    Updated font style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ function Banner(){
                         <div className="banner-content">
                             <span>Kadena Artwork</span>
                             <h1>Brings to you NFT’s with great utilities.</h1>
-                            <p>Brings to you NFT’s with great utilities. Each NFT's launch will have utilities linked with each other. This will assist with leveraging each to continue to bring more to the holders within our community.</p>
+                            <p className="fw-semibold">Brings to you NFT’s with great utilities. Each NFT's launch will have utilities linked with each other. This will assist with leveraging each to continue to bring more to the holders within our community. </p>
                         </div>
                     </div>
                 </div>
