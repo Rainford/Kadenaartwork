bfff2aab0eb11099ec561e886f65a70f56bb4a5a
Rainford Lennon <rainfordlennon@gmail.com>
2023-01-09 23:35:23 -0500

    Updated link to open in new tab

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -6,7 +6,7 @@ function SocialLinks(){
         
         <ul className="social-links d-flex align-items-center ms-xl-5 ms-lg-4">
             <li><a href="/"><FontAwesomeIcon icon={faFacebookF} /></a></li>
-            <li><a href="https://discord.com/invite/7KKJSKCWSn"><FontAwesomeIcon icon={faDiscord} /></a></li>
+            <li><a href="https://discord.com/invite/7KKJSKCWSn" target="_blank"><FontAwesomeIcon icon={faDiscord} /></a></li>
             <li><a href="/"><FontAwesomeIcon icon={faTelegramPlane} /></a></li>
             <li><a href="/"><FontAwesomeIcon icon={faTwitter} /></a></li>
             
